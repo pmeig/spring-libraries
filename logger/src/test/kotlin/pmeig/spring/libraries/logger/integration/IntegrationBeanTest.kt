1b3d17e69d9398f0beeb91f7f5e82704e2b4c6fb
pmeig <pierrick.workplace@gmail.com>
2026-06-09 20:10:18 +0200

    try to not error when module has no test

--- logger/src/test/kotlin/pmeig/spring/libraries/logger/integration/IntegrationBeanTest.kt
+++
@@ -1,8 +0,0 @@
-package pmeig.spring.libraries.logger.integration
-
-import org.junit.jupiter.api.extension.ExtendWith
-import org.mockito.junit.jupiter.MockitoExtension
-
-@ExtendWith(MockitoExtension::class)
-class IntegrationBeanTest {
-}
